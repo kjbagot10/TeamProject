e7f84b249a1428abd75fa2a0a17871f0df88870d
kjbagot10 <151742506+kjbagot10@users.noreply.github.com>
2024-11-08 18:14:14 +0000

    Repeatability functions and sort function
    Need to figure how to add to end of SQL Query if they want to order by multiple things. Also need to remember how to get values from a button that is pressed.

--- functions.php
+++ functions.php
@@ -1,23 +1,22 @@
 <?php
 // need to test this but I think it should work. 
-function dynListDesc($dbConn, $catIDToCompare)
+function preDefListDyn($dbConn, $catIDToCompare)
 { 
     try {
         // sql query
         $sqlCatDescQuery = "SELECT item_id, user_id, expiry_date, item_name, date_added, storage_type FROM inventory_items"; 
         $sqlResults = $dbConn->query($sqlCatDescQuery);    
-
-        while ($item = $sqlResults->fetchObject()) {
+        echo "<ul class='menu' id='listOfItems'>";
+        while ($item = $sqlResults->fetchObject()) 
+        {
             
-            echo "\t<div class='item'>
+            echo "<li class='menu-item'>
                     <span class='item_name'>{$item->item_name}</span>
                     <span class='category'>{$item->category}</span>
                     <span class='expiry_date'>{$item->expiry_date}</span>
-                    <span class='date_added'>{$item->date_added}</span>
-                    <span class='storage_type'>{$item->storage_type}</span>
-                    </div>\n";
-            }
-        
+                  </li>\n";
+        }
+        echo "</ul>";   
 
     } catch (Exception $e)
     {
@@ -25,6 +24,58 @@ function dynListDesc($dbConn, $catIDToCompare)
     }
 }
 
+// this could be used for the predefined list
+// could remove the query from this and add it to the sortTable Func. 
+function dynTable($dbConn)
+{
+    
+    try {
+        $sqlCatDescQuery = "SELECT item_id, user_id, expiry_date, item_name, date_added, storage_type FROM inventory_items"; 
+        $sqlResults = $dbConn->query($sqlCatDescQuery);
+        // the column names for the user. 
+        echo "
+        <thead>
+            <tr>
+            <th><abbr title='Item Name'>Name</abbr></th>
+                <th>Team</th>
+                <th><abbr title='Expiry Date'>Exp Date</abbr></th>
+                <th><abbr title='Storage Method'>Strg Meth</abbr></th>
+                <th><abbr title='Type'>Typ</abbr></th>
+            
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+        ";
+        // will dynamically create the table cells
+        while ($item = $sqlResults->fetchObject())
+        {
+        echo 
+        "
+        <tr>
+        <th>{$item->item_name}</th>
+            <td>{$item->expiry_date}</td>
+            <td>{$item->storage_type}</td>
+            <td>{$item->category}</td>
+        </tr>  
+            
+        ";
+        }
+        
+         
 
+    }
+    catch (Exception $e)
+    {
+        throw new Exception("Error creating".$e.getMessage(), 0, $e);
+    }
+}
+//-- use this is basically the outline of the finished function
+function sortTableFunc($dbConn)
+{
+    // $value = val of button pressed. Need to figure how I wll do this.
+    // Need to figure how to add to the end of the string. 
+    $sqlCatDescQuery = "SELECT item_id, user_id, expiry_date, item_name, date_added, storage_type FROM inventory_items ORDER BY {$value}"; 
 
+}
 ?>
