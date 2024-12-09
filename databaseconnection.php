ae2cea8d55bf48120f346ac56bc1aa586c17a329
dimm2208 <lequyen.nguyen2208@gmail.com>
2024-12-09 12:32:03 +0000

    Update databaseconnection.php
    I want to delete this file

--- databaseconnection.php
+++ databaseconnection.php
@@ -1,13 +1,1 @@
-<?php
-$host = 'nuwebspace';
-$dbname = 'w21018460';
-$username = 'w21018460';
-$password = 'bpRAtdZ8';
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
-}
-?>
